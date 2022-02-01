4bcfe1058e7e8ae99f91431a60e4e30aba0bc163
TarasDutchak <tarasdutchak90@gmail.com>
2022-02-01 16:46:38 +0200

    fix tel inputs

--- js/common.js
+++ js/common.js
@@ -73,7 +73,7 @@ $(document).ready(function () {
     // input type TEL
     if ($(".phoneinp").length) {
         $('.phoneinp').inputmask({
-            "mask": "99 /999/ 9999999",
+            "mask": "+99 /999/ 9999999",
             showMaskOnHover: false,
             showMaskOnFocus: false,
         });
